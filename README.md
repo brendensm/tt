3894fd68e10dd94d0ef95107742a0dcace8dc890
brendensm <brendensmithmi@gmail.com>
2025-06-16 08:36:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## tt -- A TidyTuesday Command Line Utility
+
+This repo contains a simple script to streamline the creation of weekly TidyTuesday scripts.
+
+To install the script with one line, run this in bash/zsh:
+
+```
+curl -sSL https://raw.githubusercontent.com/brendensm/tt/main/tt.sh -o ~/.local/bin/tt && chmod +x ~/.local/bin/tt
+```
+
---
+++ tt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+WEEK=${1:-$(date +%W)}
+YEAR=${2:-$(date +%G)}
+
+FILENAME="tt_${WEEK}_${YEAR}.R"
+
+
+if [ -e "$FILENAME" ]; then
+    echo "File '$FILENAME' already exists."
+
+elif [ "$WEEK" -lt 1 ] || [ "$WEEK" -gt 53 ]; then
+    echo "Please enter a valid week (1-53)."
+
+elif [ "$YEAR" -lt 2018 ] || [ "$YEAR" -gt "$(date +%G)" ]; then
+    echo "Please enter a valid year (2018-$(date +%G))."
+
+else
+    echo "library(tidyverse)
+    library(tidytuesdayR)
+
+    tuesdata <- tt_load(${YEAR}, ${WEEK})
+
+    list2env(tuesdata, envir = .GlobalEnv)" > "$FILENAME"
+
+    chmod +x "$FILENAME"
+    echo "Created '$FILENAME'"
+fi
+
+
